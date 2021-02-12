310887a6bc2f230388b44a45d776aade9f9b4cf7
Vj <Vijayanand.Chinnakannan@hotmail.com>
2021-02-11 21:24:14 -0500

    Initial Commit

---
+++ SwiftListViewArray/SwiftListViewArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftListViewArrayApp.swift
+//  SwiftListViewArray
+//
+//  Created by Vijayanand Chinnakannan on 2021-02-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftListViewArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
